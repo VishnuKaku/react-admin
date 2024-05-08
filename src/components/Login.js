2560019df39645b561a5bf62846410d4e05c8eea
Faisal Iqbal <iqbalf203@gmail.com>
2024-05-08 09:31:17 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useState } from 'react';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
